86eceefa9d6b9d577a478be672e8f3cce03e7377
sunff <sunff@sunffdeMacBook-Pro.local>
2018-05-15 21:59:06 +0800

    空指针修复

--- jeesite-sys/jeesite-sys-rt/src/main/java/com/thinkgem/jeesite/modules/sys/cas/service/impl/CasUserServiceImpl.java
+++ jeesite-sys/jeesite-sys-rt/src/main/java/com/thinkgem/jeesite/modules/sys/cas/service/impl/CasUserServiceImpl.java
@@ -26,49 +26,54 @@ import com.z.jeesite.sys.api.dto.CasUser;
 @Service
 public class CasUserServiceImpl implements CasUserService {
 
-	private static final Logger logger = LoggerFactory.getLogger(CasUserServiceImpl.class);
-	@Autowired
-	private SystemService systemService;
+    private static final Logger logger = LoggerFactory.getLogger(CasUserServiceImpl.class);
+    @Autowired
+    private SystemService systemService;
 
-	@Autowired
-	private MenuDao menuDao;
+    @Autowired
+    private MenuDao menuDao;
 
-	@Override
-	public CasUser getUserByLoginName(String loginName) {
-		User user = systemService.getUserByLoginName(loginName);
-		CasUser casUser = new CasUser();
-		casUser.setId(user.getId());
-		casUser.setLoginFlag(user.getLoginFlag());
-		casUser.setLoginName(user.getLoginName());
-		casUser.setName(user.getName());
+    @Override
+    public CasUser getUserByLoginName(String loginName) {
+        User user = systemService.getUserByLoginName(loginName);
+        if (user == null) {
+            return null;
+        }
+        CasUser casUser = new CasUser();
+        casUser.setId(user.getId());
+        casUser.setLoginFlag(user.getLoginFlag());
+        casUser.setLoginName(user.getLoginName());
+        casUser.setName(user.getName());
 
-		List<Menu> menuList = null;
-		User currentUser = UserUtils.getUser();
-		if (currentUser.getId() == null || !currentUser.getId().equals(user.getId())) {
-			logger.info("无法直接获取当前用户信息，loginname={}", loginName);
-			if (user.isAdmin()) {
-				menuList = menuDao.findAllList(new Menu());
-			} else {
-				Menu m = new Menu();
-				m.setUserId(user.getId());
-				menuList = menuDao.findByUserId(m);
-			}
+        List<Menu> menuList = null;
+        User currentUser = UserUtils.getUser();
+        if (currentUser == null || currentUser.getId() == null || !currentUser.getId().equals(user.getId())) {
+            logger.info("无法直接获取当前用户信息，loginname={}", loginName);
+            if (user.isAdmin()) {
+                menuList = menuDao.findAllList(new Menu());
+            } else {
+                Menu m = new Menu();
+                m.setUserId(user.getId());
+                menuList = menuDao.findByUserId(m);
+            }
 
-		} else {
-			logger.info("可以获取到当前用户信息...,loginname={}", loginName);
-			menuList = UserUtils.getMenuList();
-		}
+        } else {
+            logger.info("可以获取到当前用户信息...,loginname={}", loginName);
+            menuList = UserUtils.getMenuList();
+        }
 
-		if (menuList != null && menuList.size() > 0) {
-			for (Menu menu : menuList) {
-				casUser.addPermission(menu.getPermission());
-			}
-		}
-		for (Role role : user.getRoleList()) {
-			casUser.addRoleEnName(role.getEnname());
-		}
+        if (menuList != null && menuList.size() > 0) {
+            for (Menu menu : menuList) {
+                casUser.addPermission(menu.getPermission());
+            }
+        }
+        if (user.getRoleList() != null) {
+            for (Role role : user.getRoleList()) {
+                casUser.addRoleEnName(role.getEnname());
+            }
+        }
 
-		return casUser;
-	}
+        return casUser;
+    }
 
 }
